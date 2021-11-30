2b44eda2144993bd72c13e4e5e9b8f828a5f9fd9
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-11-30 19:46:09 +0900

    Interface test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+interface Human {
+  //interface는 typescript에서만 작동한다. js에서는 작동하지않음
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "Sky",
+  age: 30,
+  gender: "male",
 };
 
-console.log(sayHi("Sky", 333, "male"));
+// const sayHi = (name: string, age: number, gender: string): string => {
+//   return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+// };
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 
 export {};
